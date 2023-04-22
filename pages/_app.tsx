10e4684f6d0516dfae2c7fd3382961c8620a3120
Philani Ncube <ncbphi001@gmail.com>
2023-04-22 08:36:42 +0200

    gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,43 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from "react";
 import { Fragment } from 'react'
+import Script from "next/script";
 import Navbar from '../components/layout/Navbar'
 import { Fraunces, Barlow } from "@next/font/google";
+import { GTM_ID, pageview } from "../lib/gtm";
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
   return (
-         <Fragment>
-              <Navbar />
-              <Component {...pageProps} />
-         </Fragment>
-         )
+    <Fragment>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
+      <Navbar />
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 export default MyApp
